e70c4d4ebb8f4d5f71c6f823666166f6a97435e3
alexander <alvox.in@gmail.com>
2018-06-14 20:10:07 -0300

    Compose view programmatically

--- allvox-app/Controller/RecorderVC.swift
+++ allvox-app/Controller/RecorderVC.swift
@@ -10,31 +10,50 @@ import UIKit
 
 class RecorderVC: UIViewController {
     
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
-    @IBOutlet weak var recBtn: UIButton!
-    
+    var recBtn: UIButton!
+    var spinner: UIActivityIndicatorView!
     let recorder = Recorder()
+    
+    let recBtnAttr: [NSAttributedStringKey: Any] = [
+        .font: UIFont(name: "AvenirNext-Bold", size: 40)!,
+        .foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.7464011312, green: 0.8857318759, blue: 0.7402122021, alpha: 1)
+        addRecBtn()
+        addSpinner()
+    }
+
+    func addRecBtn() {
+        let x = view.frame.width / 2 - 60
+        let y = view.frame.height - 200
+        recBtn = UIButton(frame: CGRect(x: x, y: y, width: 120, height: 80))
+        recBtn.setAttributedTitle(NSAttributedString(string: "REC", attributes: recBtnAttr), for: .normal)
+        recBtn.addTarget(self, action: #selector(recBtnPressed(_:)), for: .touchUpInside)
+        view.addSubview(recBtn)
+    }
+    
+    func addSpinner() {
+        let x = view.frame.width / 2 - 50
+        let y = view.frame.height / 2 - 50
+        spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        spinner.frame = CGRect(x: x, y: y, width: 100, height: 100)
         spinner.isHidden = true
+        view.addSubview(spinner)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    @IBAction func recBtnPressed(_ sender: Any) {
+    
+    @objc func recBtnPressed(_ sender: Any) {
         if recorder.isRecording {
             recorder.stopRecording()
             spinner.isHidden = true
             spinner.stopAnimating()
-            recBtn.setTitle("REC", for: .normal)
+            recBtn.setAttributedTitle(NSAttributedString(string: "REC", attributes: recBtnAttr), for: .normal)
         } else {
             recorder.startRecording()
             spinner.isHidden = false
             spinner.startAnimating()
-            recBtn.setTitle("STOP", for: .normal)
+            recBtn.setAttributedTitle(NSAttributedString(string: "STOP", attributes: recBtnAttr), for: .normal)
         }
     }
     
