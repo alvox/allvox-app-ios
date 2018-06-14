8ab5cb5a528e2563f80971477855bce33a289a72
alexander <alvox.in@gmail.com>
2018-06-14 20:42:59 -0300

    Refactor recording logic from vc to the Recorder class

--- allvox-app/Recorder.swift
+++ allvox-app/Recorder.swift
@@ -7,23 +7,71 @@
 //
 
 import Foundation
+import AVFoundation
 
-class Recorder {
+class Recorder: NSObject, AVAudioRecorderDelegate {
+    
+    private var session: AVAudioSession!
+    private var audioRecorder: AVAudioRecorder!
     
     private(set) public var isRecording: Bool
     
-    init() {
+    private let audioFileSettings = [
+        AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+        AVSampleRateKey: 12000,
+        AVNumberOfChannelsKey: 1,
+        AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+    ]
+    
+    override init() {
         self.isRecording = false
+        super.init()
     }
     
     func startRecording() {
         isRecording = true
         print("Start recoding")
+        let audioFileName = newRecordingFileName()
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFileName, settings: audioFileSettings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+        } catch {
+            debugPrint(error)
+            stopRecording(success: false)
+        }
     }
     
-    func stopRecording() {
+    func stopRecording(success: Bool) {
         isRecording = false
-        print("Stop recording")
+        audioRecorder.stop()
+        audioRecorder = nil
+        if success {
+            print("Recording was successful")
+        } else {
+            print("Recording attempt failed!!!")
+        }
+    }
+    
+    func prepare() {
+        session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.setActive(true)
+            session.requestRecordPermission() { (allowed) in
+                if allowed {
+                    print("Recording permissions granted")
+                }
+            }
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            stopRecording(success: false)
+        }
     }
     
 }
--- allvox-app/Util/FileUtil.swift
+++ allvox-app/Util/FileUtil.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+func newRecordingFileName() -> URL {
+    // todo: generate file name based on timestamp
+    return getDocumentsDirectory().appendingPathComponent("test-recording.m4a")
+}
