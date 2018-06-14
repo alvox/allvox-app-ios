5bbf882b4b3cca04ea07bdb42248f55e027bb6d8
alexander <alvox.in@gmail.com>
2018-06-13 21:33:37 -0300

    Simple recording functionality

--- allvox-app/Controller/RecorderVC.swift
+++ allvox-app/Controller/RecorderVC.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecorderVC: UIViewController {
+class RecorderVC: UIViewController, AVAudioRecorderDelegate {
     
     @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var recBtn: UIButton!
     
     let recorder = Recorder()
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         spinner.isHidden = true
+        
+        //
+        recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { (allowed) in
+                if allowed {
+                    print("Recording permissions granted")
+                }
+            }
+        } catch {
+            debugPrint(error)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,14 +46,55 @@ class RecorderVC: UIViewController {
     @IBAction func recBtnPressed(_ sender: Any) {
         if recorder.isRecording {
             recorder.stopRecording()
+            finishRecording(success: true)
             spinner.isHidden = true
             spinner.stopAnimating()
             recBtn.setTitle("REC", for: .normal)
         } else {
             recorder.startRecording()
+            startRecording()
             spinner.isHidden = false
             spinner.startAnimating()
             recBtn.setTitle("STOP", for: .normal)
+        }
+    }
+    
+    func startRecording() {
+        let audioFileName = getDocumentsDirectory().appendingPathComponent("rest-recording.m4a")
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFileName, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+        } catch {
+            debugPrint(error)
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        if success {
+            print("Recording was successful")
+        } else {
+            print("Recording attempt failed!!!")
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
         }
     }
     
